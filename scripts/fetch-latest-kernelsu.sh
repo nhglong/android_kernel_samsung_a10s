e5c666cad6159e6821b537c8a275914e6d0e1da4
Nguyễn Long <dmgogle123@gmail.com>
2025-07-25 13:11:48 +0000

    scripts/fetch-latest-kernelsu: Switch to KernelSU Next
    Change-Id: I92c80a7f7d7d2fb06e644e97ad6a1ef1451c7684

--- scripts/fetch-latest-kernelsu.sh
+++ scripts/fetch-latest-kernelsu.sh
@@ -4,8 +4,8 @@ exec 9>.kernelsu-fetch-lock
 flock -n 9 || exit 0
 [[ $(( $(date +%s) - $(stat -c %Y "drivers/kernelsu/.check" 2>/dev/null || echo 0) )) -gt 86400 ]] || exit 0
 
-AUTHOR="tiann"
-REPO="KernelSU"
+AUTHOR="KernelSU-Next"
+REPO="KernelSU-Next"
 VERSION=`curl -s -I -k "https://api.github.com/repos/$AUTHOR/$REPO/commits?per_page=1" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p'`
 
 if [[ -f drivers/kernelsu/.version && *$(cat drivers/kernelsu/.version)* == *$VERSION* ]]; then
@@ -17,6 +17,6 @@ fi
 rm -rf drivers/kernelsu
 mkdir -p drivers/kernelsu
 cd drivers/kernelsu
-wget -q -O - https://github.com/$AUTHOR/$REPO/archive/refs/heads/main.tar.gz | tar -xz --strip=2 "$REPO-main/kernel"
+wget -q -O - https://github.com/$AUTHOR/$REPO/archive/refs/heads/next.tar.gz | tar -xz --strip=2 "$REPO-next/kernel"
 echo $VERSION >> .version
 touch .check
